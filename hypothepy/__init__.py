3142b286fb94d0dfdfcd29dc7ee4c854144ecd70
Alejandro Riera <ariera@gmail.com>
2019-09-24 18:08:59 +0200

    Bump version: 0.2.2 → 0.3.0

--- hypothepy/__init__.py
+++ hypothepy/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """EMBO"""
 __email__ = 'embo_dev@embo.org'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/embo-press/hypothepy',
-    version='0.2.2',
+    version='0.3.0',
     zip_safe=False,
 )
